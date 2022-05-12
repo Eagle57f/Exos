22fc150b1ef0b88c5dea882f62fc85c114c83c3e
Eagle57 <94141086+Eagle57f@users.noreply.github.com>
2022-05-12 16:59:20 +0200

    Add files via upload

---
+++ Python/seance_2.py
@@ -0,0 +1,29 @@
+#Exercice 1:
+#A=3
+#B=4
+
+#Exercice 2:
+#A=2
+#B=3
+#C=1
+
+#Exercice 3:
+#A=6
+#B=2
+
+#Exercice 4: 
+#A=13
+#B=13
+#C=13
+
+#Exercice 5:
+
+def Ex5(A, B):
+    C=A
+    A=B
+    B=C
+    return A, B
+
+
+#Exercice 6:
+#C="42312"
